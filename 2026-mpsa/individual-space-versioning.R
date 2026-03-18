337308956d6c21fda2f1af799af1da6a7ad07760
Rick Morgan <rick.morgan2@gmail.com>
2026-03-18 18:01:38 -0400

    made compare_versions_fun which makes a set of boxplots comparing country-year coding of v2xcl_rol across versions.

--- 2026-mpsa/individual-space-versioning.R
+++ 2026-mpsa/individual-space-versioning.R
@@ -7,9 +7,14 @@ library(glue)
 library(readr)
 library(dplyr)
 library(tidyr)
+library(stringr)
+library(purrr)
 library(states)
 library(ggplot2)
 library(patchwork)
+
+library(ggridges)
+library(plotly)
 
 # Just do one of the more recent years, and I want to be able to make statements
 # about "looking next year, in 2 years", so we need a 3-version slice of data.
@@ -20,8 +25,8 @@ library(patchwork)
 dv_files <- paste0("2026-mpsa/input/", glue("dv-data_v{v}_ert-lite.rds", v=13:15))
 ert_lite <- dv_files |>
   map(\(path) {
-    version <- str_extract(path, "v[0-9]{1,2}") |> str_remove("v")
-
+    # version <- str_extract(path, "v[0-9]{1,2}") |> str_remove("v")
+    version <- gsub(".*v([0-9]{1,2}).*", "\\1", path)
     dv <- read_rds(path)
     dv <- dv |> filter(year > 2015)
     dv$vdem_version <- version
@@ -41,12 +46,14 @@ ert_lite <- ert_lite |>
   mutate(country_name = sapply(gwcode, lookup_country_name))
 
 
-# index_data: this is the VDem 13, 14, 15 data on v2xcl_rol (individual space),
+# index_data: this is the VDem 9 through 15 data on v2xcl_rol (individual space),
 # from 2016 onwards.
-vdem_files <- glue("create-data/input/V-Dem-CY-Full+Others-v{v}.rds", v = 13:15)
+v <- c(as.character(9:10), "11.1", as.character(12:15))
+vdem_files <- glue("create-data/input/V-Dem-CY-Full+Others-v{v}.rds")
 index_data <- vdem_files |>
   map(\(path) {
-    version <- str_extract(path, "v[0-9]{1,2}") |> str_remove("v")
+    # version <- str_extract(path, "v[0-9]{1,2}") |> str_remove("v")
+    version <- gsub(".*v([0-9]{1,2}).*", "\\1", path)
 
     vdem <- read_rds(path) |>
       as_tibble()
@@ -88,11 +95,12 @@ index_data <- vdem_files |>
 # (The OG ERT has a longer look-ahead period, I changed it to just 1-year for
 # ERT-lite, because of this "we coded the wrong value this year" issue. It means
 # that some of the 0 codings for ERT-lite in the last year of data will flip
-# when next year's data is avaiable, even if there are no changes due to
+# when next year's data is available, even if there are no changes due to
 # versioning.)
 #
 # So I'm looking at 2021 to avoid spurious changes that are not actually due to
 # V-Dem versioning.
+
 ert_lite |>
   filter(year==2021) |>
   group_by(gwcode, country_name, year) |>
@@ -209,6 +217,87 @@ ggsave("~/Desktop/individual-pakistan.png")
 # Germany, low low low
 plot_ert_lite_and_index(260) + plot_annotation(title = "Germany")
 
+# RM start ####
 
+
+# Fixed year, e.g. 2018, v2xcs_rol, if you compare v9 to v10, what is the distribution of differences; v9 to v11, ..., v10 to v11, ... etc.
+# How much can I expect the most recent year of data to change next year? Take v9 last year, compare it to v10 same year; take v10 last year, compare it to v11 same year; etc.
+
+index_data_wide <- index_data |>
+  pivot_wider(id_cols = c(gwcode, year), names_from = "vdem_version", names_prefix = "v", values_from = "v2xcl_rol") |>
+  select(gwcode, year, v9, v10, v11, v12, v13, v14, v15)
+
+# All unique pairs of version columns
+version_cols <- paste0("v", 9:15)
+pairs <- combn(version_cols, 2, simplify = FALSE)
+
+# Compute pairwise differences and bind as new columns
+diff_cols <- pairs |>
+  map(\(p) index_data_wide[[p[1]]] - index_data_wide[[p[2]]]) |>         # compute difference for each pair
+  set_names(map_chr(pairs, \(p) paste0(p[1], "_minus_", p[2]))) |>  # name "v9_minus_v10"
+  as_tibble()
+diff_col_names <- names(diff_cols)
+
+index_data_difs <- bind_cols(index_data_wide, diff_cols) |>
+  mutate(country_name = map_chr(gwcode, lookup_country_name),
+         last_yr_diff = coalesce(!!!syms(diff_col_names))) |>
+  select(country_name, gwcode, year, last_yr_diff, all_of(diff_col_names), everything()) |>
+  filter(year > 2017) # 2018 is the last reported year in v9
+
+version_last_yr <- index_data_difs |>
+  summarise(across(all_of(version_cols), \(x) max(year[!is.na(x)])))
+
+index_data_difs_long <- index_data_difs  |>
+  select(country_name, gwcode, year, last_yr_diff, all_of(diff_col_names)) |>
+  pivot_longer(cols = -c(country_name, gwcode, year), names_to = "version_diff") |>
+  filter(!is.na(value))
+
+# Let's just look at v9 relative to others for 2018
+
+compare_versions_fun <- function(dat, ref_version = "v9", version_last_yr, interactive = F){
+  use_version_last_yr <- as.numeric(version_last_yr[ref_version]) # this is made above. sloppy but ehh
+
+  title_text <- "Comparing country-year coding of v2xcl_rol across versions"
+  subtitle_text <- paste0("Year: ", use_version_last_yr, " | Reference version: ", ref_version)
+
+  use_dat <- dat |>
+    filter(str_detect(version_diff, paste0(ref_version, "_")),
+           year == use_version_last_yr)
+
+
+  if (!interactive) {
+    return(
+      use_dat |>
+        ggplot(aes(x = version_diff, y = value, fill = version_diff)) +
+        geom_boxplot(alpha = 0.7, show.legend = FALSE) +
+        labs(x = NULL, y = "Difference") +
+        plot_annotation(title = title_text,
+                        subtitle = subtitle_text) +
+        theme_minimal()
+    )
+  }
+
+  plot_ly(use_dat,
+          x = ~version_diff,
+          y = ~value,
+          color = ~version_diff,
+          type = "box",
+          boxpoints = "suspectedoutliers",
+          text = ~country_name,
+          showlegend = FALSE) |>
+    layout(
+      title = list(
+        text = paste0("<b>", title_text, "</b><br><sup>", subtitle_text, "</sup>"),
+        x = 0
+      ),
+      xaxis = list(title = ""),
+      yaxis = list(title = "Difference")
+    )
+}
+
+use_version <- version_cols[1]
+compare_versions_fun(index_data_difs_long, ref_version = use_version, version_last_yr, interactive = T)
+
+plot_ert_lite_and_index(517)+ plot_annotation(title = "Rwanda")
 
 
